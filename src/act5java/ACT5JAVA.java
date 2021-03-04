6113901d4f178005d1b2f95802bf31b6912c52fb
LDranzer <ldranzer4@gmail.com>
2021-03-03 22:07:35 -0600

    Initial commit

---
+++ src/act5java/ACT5JAVA.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package act5java;
+
+/**
+ *
+ * @author LDranzer
+ */
+public class ACT5JAVA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        double valX=10, valY=2;
+        
+        System.out.println("Suma:" + (valX+valY));
+        System.out.println("Resta:" + (valX-valY));
+        System.out.println("Multiplicación:" + (valX*valY));
+        System.out.println("División:" + (valX/valY));
+        System.out.println("Módulo:" + (valX%valY));
+        
+        
+    }
+    
+}
